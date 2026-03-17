ba26ca78a45d17bf8a200fcb689d88d8aba0edde
Ashley Caselli <ashley.caselli@unige.ch>
2026-03-17 11:12:06 +0100

    docs: add package-info.java for nanopublication test suite documentation

---
+++ src/main/java/org/nanopub/testsuite/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the code for the nanopublication test suite, which is a set of nanopublications that are used to test the functionality of nanopublication tools and libraries.
+ * The test suite includes nanopublications that cover a wide range of scenarios, including valid, invalid, signed, unsigned and more nanopublications.
+ */
+package org.nanopub.testsuite;
